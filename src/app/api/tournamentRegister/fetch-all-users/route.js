4bc3c141eccaac16caf8a18c71071df33e7c4ad3
Muhammad Fareed <144031250+Muhammadfareed78@users.noreply.github.com>
2025-09-11 21:31:25 +0500

    fetch all registeration user

---
+++ src/app/api/tournamentRegister/fetch-all-users/route.js
@@ -0,0 +1,25 @@
+import { Registration } from "@/models/Registration";
+import { ApiResponse } from "@/utils/server/ApiResponse";
+import { asyncHandler } from "@/utils/server/asyncHandler";
+import { requireAdmin } from "@/utils/server/roleGuards";
+import "@/models/Game";
+
+export const GET = asyncHandler(async (req, context) => {
+  await requireAdmin();
+
+  const fetchAllRegisterdUsersInTournament = await Registration.find()
+    .populate("user")
+    .populate({
+      path: "gameRegistrationDetails.games",
+      model: "Game",
+    })
+    .populate("tournament");
+
+  return Response.json(
+    new ApiResponse(
+      200,
+      fetchAllRegisterdUsersInTournament,
+      "Registrations fetched successfully"
+    )
+  );
+});
