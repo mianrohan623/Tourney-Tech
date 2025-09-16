f4b81cfe77343f613f44cdbb4b8f285f86b7bef9
Muhammad Fareed <144031250+Muhammadfareed78@users.noreply.github.com>
2025-09-16 20:14:13 +0500

    registration games API changes

--- src/app/api/tournaments/registration-tournament/[id]/route.js
+++ src/app/api/tournaments/registration-tournament/[id]/route.js
@@ -2,15 +2,18 @@ import { Registration } from "@/models/Registration";
 import { ApiResponse } from "@/utils/server/ApiResponse";
 import { asyncHandler } from "@/utils/server/asyncHandler";
 import "@/models/Game";
+import { requireAuth } from "@/utils/server/auth";
 
 export const GET = asyncHandler(async (_, context) => {
   const id = context.params?.id;
+  const user = await requireAuth();
 
   if (!id) throw new ApiResponse(400, null, "ID parameter is missing");
 
-  const RegisteredGames = await Registration.find({ tournament: id }).populate(
-    "gameRegistrationDetails.games"
-  );
+  const RegisteredGames = await Registration.find({
+    tournament: id,
+    user: user?._id,
+  }).populate("gameRegistrationDetails.games");
   return Response.json(
     new ApiResponse(
       200,
