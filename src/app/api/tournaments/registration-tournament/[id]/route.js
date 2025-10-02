6cde9d2fc8dc46c63bc44602b69d6aa55d9678b8
Muhammad Fareed <144031250+Muhammadfareed78@users.noreply.github.com>
2025-10-03 01:29:57 +0500

    update fetch games API

--- src/app/api/tournaments/registration-tournament/[id]/route.js
+++ src/app/api/tournaments/registration-tournament/[id]/route.js
@@ -4,6 +4,7 @@ import { asyncHandler } from "@/utils/server/asyncHandler";
 import "@/models/Game";
 import { requireAuth } from "@/utils/server/auth";
 import { Tournament } from "@/models/Tournament";
+import { Team } from "@/models/Team";
 
 export const GET = asyncHandler(async (_, context) => {
   const id = context.params?.id;
@@ -16,12 +17,15 @@ export const GET = asyncHandler(async (_, context) => {
   if (user?.role === "admin") {
     registeredGames = await Tournament.findById(id).populate("games.game");
   } else {
-    registeredGames = await Registration.find({
+    const existUserTeam = await Team.find({
       tournament: id,
-      user: user?._id,
-    }).populate("gameRegistrationDetails.games");
-  }
+      members: { $in: [user?._id] },
+    });
 
+    if (existUserTeam) {
+      registeredGames = await Tournament.findById(id).populate("games.game");
+    }
+  }
 
   return Response.json(
     new ApiResponse(
